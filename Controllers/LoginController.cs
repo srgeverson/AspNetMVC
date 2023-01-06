a1b5916bf7448bbe591bb8c6bdd8f813a2f23338
geverson.souza <geverson.souza@odontosystem.com.br>
2023-01-06 15:54:45 -0300

    Controle de acesso

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,7 +1,9 @@
 ﻿using AspNetMVC.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Security.Claims;
 
 namespace AspNetMVC.Controllers
 {
@@ -31,15 +33,35 @@ namespace AspNetMVC.Controllers
 
         [AllowAnonymous]
         [HttpPost]
-        public void Logar(UsuarioLoginModel usuarioLoginModel)
+        public async Task Logar(UsuarioLoginModel usuarioLoginModel)
         {
-            if (usuarioLoginModel != null && usuarioLoginModel.Email != null && usuarioLoginModel.Email.Equals("geversonjosedesouza@gmail.com"))
-            {
-                ViewData["UsuarioAutenticado"] = usuarioLoginModel;
-                Response.Redirect("/Home");
-            }
-            else
-                Error(String.Format("Usuário ou senha inválidos!"));
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, Guid.NewGuid().ToString()) };
+
+            var claimsIdentity = new ClaimsIdentity(
+              claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(
+              CookieAuthenticationDefaults.AuthenticationScheme,
+              new ClaimsPrincipal(claimsIdentity),
+              authProperties);
+            
+            Response.Redirect("/Home");
+          //  return View("/Home");
+
+            //if (usuarioLoginModel != null && usuarioLoginModel.Email != null && usuarioLoginModel.Email.Equals("geversonjosedesouza@gmail.com"))
+            //{
+            //    ViewData["UsuarioAutenticado"] = usuarioLoginModel;
+            //    Response.Redirect("/Home");
+            //}
+            //else
+            //    Error(String.Format("Usuário ou senha inválidos!"));
+        }
+        public async Task Sair()
+        {
+            await HttpContext.SignOutAsync();
+            Response.Redirect("/Login");
+            //return View("Acessar");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options =>
+  {
+      options.Cookie.HttpOnly = true;
+      options.Cookie.SecurePolicy = true
+        ? CookieSecurePolicy.None : CookieSecurePolicy.Always;
+      options.Cookie.SameSite = SameSiteMode.Lax;
+      options.Cookie.Name = "SimpleTalk.AuthCookieAspNetCore";
+      options.LoginPath = "/Login";
+      options.LogoutPath = "/Home";
+  });
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Strict;
+    options.HttpOnly = HttpOnlyPolicy.None;
+    options.Secure = true
+      ? CookieSecurePolicy.None : CookieSecurePolicy.Always;
+});
+
+builder.Services.AddMvc(options => options.Filters.Add(new AuthorizeFilter()));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+app.UseCookiePolicy();
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,16 +52,17 @@ app.UseAuthorization();
 #region Rotas
 
 //app.MapControllerRoute(
+//    name: "sair",
+//    pattern: "{controller=Login}/{action=Sair}");
+
+app.MapControllerRoute(
+name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+//app.MapControllerRoute(
 //    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "logar",
-    pattern: "{controller=Login}/{action=Logar}");
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+//    pattern: "{controller=Login}/{action=Index}/{id?}");
 #endregion
 
 app.Run();
